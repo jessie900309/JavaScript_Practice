0625bd772f74ce4646adb169106c358a0eb8841e
JessieOuO <jessieouo@Xanonymous-Mac-mini.local>
2025-01-31 23:56:40 +0800

    [add] practice: 9x9 table

--- practice_src/table.js
+++ practice_src/table.js
@@ -1,23 +1,24 @@
-let num = 9;
-// let num_set = Number(prompt("enter a number (integer > 0) : ", "9"));
+const table_style = "border-collapse: collapse; text-align: center; font-family: 'Comic Sans MS', cursive; font-size: 20px; color: white;";
+const cell_style = "border: 1px solid white; width: 50px; height: 40px;";
+const num_color = "blueviolet";
 
 const table = document.createElement("table");
-table.setAttribute("style", "border-collapse: collapse; text-align: center; font-family: 'Comic Sans MS', cursive; font-size: 20px; color: white;");
+table.setAttribute("style", table_style);
 const tbody = document.createElement("tbody");
 
 // [1..9]
 const row = document.createElement("tr");
-row.setAttribute("style", "border: 1px solid white; width: 50px; height: 40px;");
+row.setAttribute("style", cell_style);
 for (let i = 0; i <= 9; i++) {
     const cell = document.createElement("td");
-    cell.setAttribute("style", "border: 1px solid white; width: 50px; height: 40px;");
+    cell.setAttribute("style", cell_style);
     let cell_block = document.createElement("span");
     let cell_value;
     if (i === 0){
         cell_value = document.createTextNode(` `);
     } else {
         cell_value = document.createTextNode(`${i}`);
-        cell_block.style.color = "blueviolet";
+        cell_block.style.color = num_color;
     }
     cell_block.appendChild(cell_value);
     cell.appendChild(cell_block);
@@ -28,15 +29,15 @@ tbody.appendChild(row);
 // [1..9][1..9]
 for (let i = 1; i <= 9; i++) {
     const row = document.createElement("tr");
-    row.setAttribute("style", "border: 1px solid white; width: 50px; height: 40px;");
+    row.setAttribute("style", cell_style);
     for (let j = 0; j <= 9; j++) {
         const cell = document.createElement("td");
-        cell.setAttribute("style", "border: 1px solid white; width: 50px; height: 40px;");
+        cell.setAttribute("style", cell_style);
         let cell_block = document.createElement("span");
         let cell_value;
         if (i*j === 0){
             cell_value = document.createTextNode(`${i}`);
-            cell_block.style.color = "blueviolet";
+            cell_block.style.color = num_color;
         } else {
             cell_value = document.createTextNode(`${i*j}`);
         }
